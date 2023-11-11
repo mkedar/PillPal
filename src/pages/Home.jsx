2a4e1ea30ade1e48d0889245ae96a5e4d5f3fcff
Oron Paz <oron.paz26@gmail.com>
2023-11-10 21:56:15 -0800

    fixed css for hamburger menu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="home">
       <div className="titlePillPalBox">
-        <h1 className="titlePillPalText">Empowering Health Through Seamless Connections...</h1>
+        <h1 className="titlePillPalText">Elevating Health Through Seamless Connections</h1>
       </div>
       <div className="explanationBox">
         <p className="explanationText">Discover PillPal - forging a stronger connection between doctors and patients for simplified healthcare. Our platform ensures easy prescription checks, health monitoring, and seamless communication. PillPal simplifies healthcare, providing a user-friendly interface for efficient medication tracking. Stay effortlessly connected with your healthcare provider, fostering a healthier and happier life.</p></div>

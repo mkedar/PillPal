d9265c38d83c585bc4f37f44d994ef8869b59e2b
Oron Paz <oron.paz26@gmail.com>
2023-11-11 15:04:10 -0800

    fixed phone overflow and added movement to pill

--- src/components/Pill.jsx
+++ src/components/Pill.jsx
@@ -1,62 +1,88 @@
 import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
+import Hammer from 'hammerjs';
 
 const Pill = () => {
   const mount = useRef(null);
-  const isMounted = useRef(true); // Keep track of whether the component is mounted
+  const isMounted = useRef(true);
+  const pill = useRef(null);
+  const camera = useRef(null);
 
   useEffect(() => {
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); //honestly not that sure what this does
+    camera.current = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     let renderer = new THREE.WebGLRenderer();
-    renderer = new THREE.WebGLRenderer({ alpha: false }); //set to true to make background transparent
+    renderer = new THREE.WebGLRenderer({ alpha: false });
 
+    const pillHeight = 1.8;
 
     // Set up the scene
-    camera.position.set(0, 0, 5);
+    camera.current.position.set(0, pillHeight / 2 + 0.8, 5);
     renderer.setSize(window.innerWidth, window.innerHeight);
     mount.current.appendChild(renderer.domElement);
 
     // Create a simple pill geometry
-    const pillGeometry = new THREE.CapsuleGeometry(0.3, 0.8, 20, 20); // {radius, height, polygon side, pilygon top circle}
+    const pillGeometry = new THREE.CapsuleGeometry(0.9, pillHeight, 20, 20);
     const pillMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff, transparent: true, opacity: 1 });
-    const pill = new THREE.Mesh(pillGeometry, pillMaterial);
+    pill.current = new THREE.Mesh(pillGeometry, pillMaterial);
 
     // Center the pill in the scene
-    pill.position.set(0, 0, 0); //moves the pill around the scene {x,y,z}
-    scene.add(pill);
+    pill.current.position.set(0, pillHeight, 0);
+    scene.add(pill.current);
 
     // Handle window resize
     const handleResize = () => {
       const { innerWidth, innerHeight } = window;
-      camera.aspect = innerWidth / innerHeight;
-      camera.updateProjectionMatrix();
+      camera.current.aspect = innerWidth / innerHeight;
+      camera.current.updateProjectionMatrix();
       renderer.setSize(innerWidth, innerHeight);
     };
 
     // Listen for window resize events
     window.addEventListener('resize', handleResize);
 
+    // Swipe interaction
+    let previousX = 0;
+
+    // Mouse swipe interaction
+    let mouseX = 0;
+    let mouseY = 0;
+    let targetRotationX = 0;
+    let targetRotationY = 0;
+    const windowHalfX = window.innerWidth / 2;
+    const windowHalfY = window.innerHeight / 2;
+
+    const onMouseMove = (event) => {
+      mouseX = (event.clientX - windowHalfX) / 2;
+      mouseY = (event.clientY - windowHalfY) / 2;
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+
     // Animation logic
     const animate = () => {
       requestAnimationFrame(animate);
-        pill.rotation.x += 0.01; // spin forward + or backwards -
-        pill.rotation.y += 0.01; // honestly not sure, only takes affect when z is affected
-        pill.rotation.z +=0.01; //makes it spin diagonally like a clock +
-      renderer.render(scene, camera);
+
+      if (pill.current) { // Check if pill is defined
+        targetRotationX += 0.05 * (mouseX - targetRotationX);
+        targetRotationY += 0.05 * (mouseY - targetRotationY);
+
+        pill.current.rotation.x = targetRotationY * 0.005;
+        pill.current.rotation.y = targetRotationX * 0.005;
+      }
+
+      renderer.render(scene, camera.current);
     };
 
     animate();
 
     // Cleanup on component unmount
     return () => {
-      // Check if the component is still mounted before removing the renderer's DOM element
       if (isMounted.current) {
         window.removeEventListener('resize', handleResize);
-        // eslint-disable-next-line
+        document.removeEventListener('mousemove', onMouseMove);
         mount.current.removeChild(renderer.domElement);
       }
-      // Update the isMounted flag to false when unmounting
       isMounted.current = false;
     };
   }, []);
--- src/pages/PillPage.jsx
+++ src/pages/PillPage.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Pill from '../components/Pill';
+import './pillpage.css';
 
 export default function PillPage() {
   return (
-    <div className='pillBox'>
-      <p>hello</p>
-      <Pill />
-      <p>Hello</p>
+    <div className='pillPage'>
+      <div className='medNameBox'>
+        <h3>[Medication Name]</h3>
+      </div>
+      <div className='pillBox'>
+        <Pill />
+      </div>
+      <div>
+        <p>Hello</p>
+      </div>
     </div>
   );
 }

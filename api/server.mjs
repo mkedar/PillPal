9ffbc5d73dd85644446abd31b1e1ce485376995d
Oron Paz <oron.paz26@gmail.com>
2023-11-14 11:01:11 -0800

    create signup API route

--- api/server.mjs
+++ api/server.mjs
@@ -1,7 +1,8 @@
-import  Express  from "express";
+import express  from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.mjs';
+import authRouter from './routes/auth.route.mjs'
 
 dotenv.config({ path: '../.env' });
 
@@ -12,10 +13,13 @@ mongoose.connect(process.env.MONGO).then(() => {
   console.log(err);
 });
 
-const app = Express();
+const app = express();
+app.use(express.json());
 
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
 })
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import  Header from './components/Header';
-import Home from './pages/Home';
-import About from './pages/About';
-import SignIn from './pages/SignIn';
-import Profile from './pages/Profile';
-import Footer from './components/Footer';
-import PillPage from './pages/PillPage';
+import  Header from './components/Header.jsx';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import SignIn from './pages/SignIn.jsx';
+import Profile from './pages/Profile.jsx';
+import Footer from './components/Footer.jsx';
+import PillPage from './pages/PillPage.jsx';
 
 
 

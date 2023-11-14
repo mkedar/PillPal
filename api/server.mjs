e88ea5a7077ae917006da0bac27f91a85e6e7ccc
Oron Paz <oron.paz26@gmail.com>
2023-11-13 23:51:43 -0800

    created test API route Working

--- api/server.mjs
+++ api/server.mjs
@@ -1,9 +1,10 @@
 import  Express  from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.mjs';
+
 dotenv.config({ path: '../.env' });
 
-console.log("Mongo: " + process.env.MONGO);
 mongoose.connect(process.env.MONGO).then(() => {
   console.log("connected to db");
 })
@@ -16,3 +17,5 @@ const app = Express();
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
 })
+
+app.use("/api/user", userRouter);

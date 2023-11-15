2a953d595eef51e610dda2358d6607a52109c478
Oron Paz <oron.paz26@gmail.com>
2023-11-14 17:03:51 -0800

    created middleware for errors

--- api/server.mjs
+++ api/server.mjs
@@ -23,3 +23,13 @@ app.listen(4000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Interntal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+  });
+});
+

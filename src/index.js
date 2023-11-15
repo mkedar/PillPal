73c40362d60e3b416e893972fc41c5a3daf1a87c
Oron Paz <oron.paz26@gmail.com>
2023-11-15 10:34:33 -0800

    localStorage for user data

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import { store } from './redux/store.js';
 import { Provider } from 'react-redux';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
@@ -15,7 +14,4 @@ root.render(
   </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,48 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice.js';
 
+// Load state from local storage
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('userState');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+// Save state to local storage
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('userState', serializedState);
+  } catch (err) {
+    // Handle errors
+  }
+};
+
+const initialState = loadState() || {
+  currentUser: null,
+  error: null,
+  loading: false,
+};
+
+const userReducerWithLocalStorage = (state = initialState, action) => {
+  const newState = userReducer(state, action);
+
+  // Save the state to local storage after each action
+  saveState(newState);
+
+  return newState;
+};
+
 export const store = configureStore({
-  reducer: {user: userReducer},
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-    serializableCheck: false,
-  }),
-})
+  reducer: { user: userReducerWithLocalStorage },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});

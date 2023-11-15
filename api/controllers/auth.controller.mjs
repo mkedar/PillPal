f267fbb346dde9ab834719d8a835c4a8c43ce75a
Oron Paz <oron.paz26@gmail.com>
2023-11-14 21:43:36 -0800

    sign in api

--- api/controllers/auth.controller.mjs
+++ api/controllers/auth.controller.mjs
@@ -1,6 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.mjs";
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config({ path: '../../.env' });
 
 export const signup = async (req, res, next) => {
   const {username, email, password} = req.body;
@@ -10,6 +13,21 @@ export const signup = async (req, res, next) => {
     await newUser.save()
     res.status(201).json("user created succesfully");
   } catch(error){
+    next(error);
+  }
+}
+
+export const signin = async (req, res, next) => {
+  const {email, password} = req.body;
+  try {
+    const validUser = await User.findOne({email});
+    if (!validUser) return next(errorHandler(404, 'This email is not associated with an account'));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if(!validPassword) return next(errorHandler(401, "Wrong credential"));
+    const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+    const { password: pass, ...rest} = validUser._doc;
+    res.cookie('access_token', token, {httpOnly: true, expires: new Date(Date.now() +24*60*60*1000)}).status(200).json(rest);
+  } catch (error){
     next(error);
   }
 }

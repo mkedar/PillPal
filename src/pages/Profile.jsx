3f078eda8928712049d3316c4308a38a3c34e2f7
Oron Paz <oron.paz26@gmail.com>
2023-11-19 20:33:52 -0800

    small code changes

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -112,7 +112,8 @@ export default function Profile() {
         <span className='text-red-700 cursor-pointer'>Sign Out</span>
       </div>
 
+      <p className='text-red-700 mt-5'>{error ? error : ''}</p>
       <p className='text-green-700 mt-5'>{updateSuccess ? 'User updated successfully' : ''}</p>
     </div>
-  )
+  );
 }
